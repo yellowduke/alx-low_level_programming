24e457e8d947d09d98b69e4e5db2fa06d04b5698
kevinmid3 <kevinmid3@gmail.com>
2022-12-08 18:29:12 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,14 @@
-#!/bin/bash
 #include <stdio.h>
 
-
-
 /**
- *
- *  *main - Entry point
- *
- *   *Return: Always 0 (Success)
- *
- *    */
-
+ * main - main block
+ * Return: 0
+ */
 
 
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
