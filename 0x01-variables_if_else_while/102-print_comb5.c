7bfad2acc768698e171d1a0857b6c66eb8781be7
yellowduke <yelohduke@gmail.com>
2022-12-09 18:21:10 -0800

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -27,7 +27,7 @@ int main(void)
 					putchar(o);
 
 					if (!((tens == '9' && ones == '8') &&
-					      (t == '9' && o == '9')))
+								(t == '9' && o == '9')))
 					{
 						putchar(',');
 						putchar(' ');
