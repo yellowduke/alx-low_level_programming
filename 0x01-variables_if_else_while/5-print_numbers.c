b4db8dd0e5ce7596d646be9245605b62426378e7
kevinmid3 <kevinmid3@gmail.com>
2022-12-12 19:23:38 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - Print all single digit numbers of bass 10 starting from 0.
+ *Return: Always 0.
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
