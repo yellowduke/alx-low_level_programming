434f7bdc3ed430390e01341803771795f659100a
kevinmid3 <kevinmid3@gmail.com>
2022-12-12 20:10:58 -0800

    Patience, persistence and perspiration

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,10 +9,10 @@ int main(void)
 {
 	int num;
 
-	for (num =0; num <= 9; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
-		if (num ==9)
+		if (num == 9)
 			continue;
 	}
 
