db483115fc9f29f762127840ea8ca76639041f3d
yellowduke <yelohduke@gmail.com>
2022-12-14 07:02:56 -0800

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,10 @@ int print_last_digit(int n)
 {
 	int last_digit = n % 10;
 
-	if (n < 0)
-		n *= -1;
+	if (last_digit < 0)
+		last_digit *= -1;
 
-	_putchar('0' + (n % 10));
+	_putchar('0' + (last_digit));
 
-	return (n % 10);
+	return (last_digit);
 }
