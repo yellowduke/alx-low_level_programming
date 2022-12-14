4550bb22cafdbf9e5fd6c5d2eed8f8c8230daf5a
yellowduke <yelohduke@gmail.com>
2022-12-14 01:04:39 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,11 @@
  * Return: 0
  */
 
-void print_alphabet(void)
+void print_alphabet(void);
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter+++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 
 	_putchar('\n');
