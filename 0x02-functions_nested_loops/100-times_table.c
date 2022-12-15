bb383fee0def5c3c71b31aa2e6b47cadcae1176b
yellowduke <yelohduke@gmail.com>
2022-12-15 14:39:20 -0800

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,36 +13,38 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (row = 0; column <= n; column++)
+		for (row = 0; row <= n; row==)
 		{
-			product = (row * column);
-			if (column == 0)
-				_putchar('0' + product);
-			else
+			for (column = 0; column <= n; column++)
 			{
-				_putchar(',');
-				_putchar(' ');
-				if (product <= 9)
+				product = (row * column);
+				if (column == 0)
+					_putchar('0' + product);
+				else
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar('0' + product);
-				}
-				else if (product > 9 && product < 100)
-				{
-					_putchar(' ');
-					_putchar('0' + (product / 10));
-					_putchar('0' + (product % 10));
-				}
-				else if (product >= 100)
-				{
-					_putchar('0' + (product / 100));
-					_putchar('0' + ((product / 10) % 10));
-					_putchar('0' + (product % 10));
+					if (product <= 9)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar('0' + product);
+					}
+					else if (product > 9 && product < 100)
+					{
+						_putchar(' ');
+						_putchar('0' + (product / 10));
+						_putchar('0' + (product % 10));
+					}
+					else if (product >= 100)
+					{
+						_putchar('0' + (product / 100));
+						_putchar('0' + ((product / 10) % 10));
+						_putchar('0' + (product % 10));
+					}
 				}
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
-
