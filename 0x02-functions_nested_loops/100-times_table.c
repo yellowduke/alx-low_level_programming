4e7dfd2ca4e2b2f0a8765a550ce39c1bfa562b7e
yellowduke <yelohduke@gmail.com>
2022-12-15 14:04:38 -0800

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,7 +15,7 @@ void print_times_table(int n)
 	{
 		for (row = 0; column <= n; column++)
 		{
-			product = (row * colimn);
+			product = (row * column);
 			if (column == 0)
 				_putchar('0' + product);
 			else
@@ -37,7 +37,7 @@ void print_times_table(int n)
 				else if (product >= 100)
 				{
 					_putchar('0' + (product / 100));
-					_putchar('0' + ((product / 10) % % 10));
+					_putchar('0' + ((product / 10) % 10));
 					_putchar('0' + (product % 10));
 				}
 			}
