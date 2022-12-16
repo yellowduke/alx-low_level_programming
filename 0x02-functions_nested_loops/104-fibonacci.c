b284d9cd48d9124404ee12e66a818edcafe2c535
yellowduke <yelohduke@gmail.com>
2022-12-16 11:50:48 -0800

    4 fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -18,7 +18,7 @@ int main(void)
 		printf("%lu, ", sum);
 
 		fib1 = fib2;
-		fib2 + sum;
+		fib2 = sum;
 	}
 
 	fib1_half1 = fib1 / 10000000000;
@@ -30,7 +30,7 @@ int main(void)
 	{
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 = fib2_half2;
-		if (fib1_half2 + fib2_-half2 > 9999999999)
+		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
 			half1 += 1;
 			half2 %= 10000000000;
