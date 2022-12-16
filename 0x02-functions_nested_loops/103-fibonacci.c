a2b4477bd76e9b5e08b4816d97f980fdd769a4ae
yellowduke <yelohduke@gmail.com>
2022-12-16 01:09:50 -0800

    3 fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -15,13 +15,14 @@ int main(void)
 		fibsum = fib1 + fib2;
 		if (fibsum > 4000000)
 			break;
+
 		if ((fibsum % 2) == 0)
 			tot_sum += fibsum;
 
 		fib1 = fib2;
 		fib2 = fibsum;
 	}
-	printf("%.o0f\n:, tot_sum);
+	printf("%.0f\n", tot_sum);
 
 	return (0);
 }
