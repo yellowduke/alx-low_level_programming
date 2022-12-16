7758c888d659baa707eb54bcdf426f1dd323162f
yellowduke <yelohduke@gmail.com>
2022-12-16 12:40:21 -0800

    4 fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,44 +7,43 @@
 
 int main(void)
 {
-	int count;
-	unsigned long fib1 = 0, fib2 = 1, sum;
-	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-	unsigned long half1, half2;
+	int count, overflow;
+	unsigned long a = 1;
+	unsigned long b = 1;
+	unsigned long sum = 0;
+	long a_head, a_tail, b_head, b_tail, sum_head, sum_tail;
 
-	for (count = 0; count < 92; count++)
+	printf("1");
+
+	for (count = 2; count < 93; count++)
 	{
-		sum = fib1 + fib2;
-		printf("%lu, ", sum);
+		sum = a + b;
+		a = b;
+		b = sum;
+		printf(", %lu", sum);
+	}
+	
+	a_head = a / 10000000000;
+	a_tail = a % 10000000000;
+	b_head = b / 10000000000;
+	b_tail = b % 10000000000;
 
-		fib1 = fib2;
-		fib2 = sum;
+	for (; count < 99; count++)
+	{
+		overflow = (a_tail + b_tail) / 1000000000;
+		sum_tail = (a_tail + b_tail) - (1000000000 * overflow);
+		sum_head = (a_head = b_head) = overflow;
+
+		printf(", %lu%lu", sum_head, sum_tail);
+
+		a_head = b_head;
+		a_tail = b_tail;
+		b_head = sum_head;
+		b_tail = sum_tail;
 	}
 
-	fib1_half1 = fib1 / 10000000000;
-	fib2_half1 = fib2 / 10000000000;
-	fib1_half2 = fib1 % 10000000000;
-	fib2_half2 = fib2 % 10000000000;
 
-	for (count = 93; count < 99; count++)
-	{
-		half1 = fib1_half1 + fib2_half1;
-		half2 = fib1_half2 = fib2_half2;
-		if (fib1_half2 + fib2_half2 > 9999999999)
-		{
-			half1 += 1;
-			half2 %= 10000000000;
-		}
-
-		printf("%lu%lu", half1, half2);
-		if (count != 98)
-			printf(", ");
-
-		fib1_half1 = fib2_half1;
-		fib1_half2 = fib2_half2;
-		fib2_half1 = half1;
-		fib2_half2 = half2;
-	}
 	printf("\n");
+
 	return (0);
 }
