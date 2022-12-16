17846da77fd048660977b56bc6ca1ca95b498f6a
yellowduke <yelohduke@gmail.com>
2022-12-16 15:18:39 -0800

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  * _isupper - checks for uppercase character
  * @c: character to be checked
- * Return: 1 if true, 0 if false
+ * Return: 1 if c is uppercase,  0 otherwise
  */
 
 int _isupper(int c)
