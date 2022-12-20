a06846a17fdc93fe0037f63cccc22835cfab765e
yellowduke <yelohduke@gmail.com>
2022-12-19 17:35:57 -0800

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,13 +7,15 @@
 
 void puts2(char *str)
 {
-	int i, len = 0;
+	int i = 0;
 
-	while (str[i++])
-		len++;
-
-	for (i = 0; i < len; i += 2)
-		_putchar(str[i]);
-
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
 	_putchar('\n');
 }
