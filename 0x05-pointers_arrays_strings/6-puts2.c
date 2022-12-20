0fa294cef7987193006a92dc2fb9208b8516f936
yellowduke <yelohduke@gmail.com>
2022-12-19 17:29:22 -0800

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+include "main.h"
+
+/**
+ * puts2 - Prints every character of a string
+ * @s: The string containing characters
+ */
+
+void puts2(char *str)
+{
+	int i, len = 0;
+
+	while (str[i++])
+		len++;
+
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
