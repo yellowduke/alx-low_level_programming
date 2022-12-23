8a5418adf5a8fb1400b739ddc6b946fff4f3fdbd
yellowduke <yelohduke@gmail.com>
2022-12-23 03:50:38 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - function
+ * @dest: char
+ * @src: char
+ * @n: number
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n);
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] = src[n])
+	{
+		dest[i] =src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
