06ff2e51a7218bc1cabf514876e4f938e5e911e9
yellowduke <yelohduke@gmail.com>
2022-12-23 12:24:18 -0800

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,19 +10,13 @@
 
 char *_strncat(char *dest, char *src, int n);
 {
-	int i = 0;
-	int j = 0;
+	int index = 0; dest_len = 0;
 
-	while (dest[i] != '\0')
-		i++;
+	while (dest[index++])
+		dest_len++;
 
-	while (src[j] = src[n])
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-	dest[i] = '\0';
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
