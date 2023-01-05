34bd88f8fa262ac0ae570711079d120bc88153ca
yellowduke <yelohduke@gmail.com>
2023-01-04 16:12:32 -0800

    Puts recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,7 +7,7 @@
  * Desc: Header file containing prototypes for all functions used in the 0x08-recursion directory.
  */
 
-int_putchar(char c);
+int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
