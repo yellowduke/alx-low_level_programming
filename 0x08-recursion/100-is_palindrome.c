7f113827bf8b13b3b79c31281bf337bb3bdbfe6b
yellowduke <yelohduke@gmail.com>
2023-01-04 17:47:01 -0800

    prime number

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,7 +9,7 @@ int is_palindrome(char *s);
  * find_strlen - Returns the length of a string.
  * @s: The string to be measured.
  *
- * Return: The lenght of the string
+ * Return: The length of the string
  */
 
 int find_strlen(char *s)
