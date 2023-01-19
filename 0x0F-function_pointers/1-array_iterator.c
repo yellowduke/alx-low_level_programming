640c8e3bad9acb48bb759dc61d1f035e6f901b89
yellowduke <yelohduke@gmail.com>
2023-01-18 17:38:17 -0800

    A goal is not always meant to be reached

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,23 +1,21 @@
 #include "function_pointers.h"
-#include <stdio.h>
 
 /**
- * array_iterator - prints each array elem on a newl
- * @array: array
- * @size: how many elem to print
- * @action: pointer to print in regular or hex 
- * Return: void
+ * array_iterator - Executes a function given as a
+ *                  parameter on each element of an array.
+ * @array: The array.
+ * @size: The size of array.
+ * @action: A pointer to the function to be executed.
  */
-
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
-
 	if (array == NULL || action == NULL)
 		return;
 
-	for (i = 0; i < size; i++)
+	while (size-- > 0)
 	{
-		action(array[i]);
+		action(*array);
+		array++;
 	}
 }
+
