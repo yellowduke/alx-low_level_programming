19579b553d1fa4f20c075722cf4e37961a345a67
yellowduke <yelohduke@gmail.com>
2023-01-26 19:02:36 -0800

    print list

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print sume of multiples of 3 or 5 below 1024
+ * main - Lists all the natural numbers below 1024 that are multiples of 3 or 5 below.
  * Return: Always 0.
  */
 
@@ -11,10 +11,8 @@ int main(void)
 
 	for (i = 0; i < 1024; i++)
 	{
-		if ((i % 3 == 0) || (1 % 5 == 0))
-		{
+		if ((i % 3) == 0 || (1 % 5) == 0)
 			sum += i;
-		}
 	}
 
 	printf("%d\n", sum);
