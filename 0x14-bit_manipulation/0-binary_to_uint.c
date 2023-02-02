f3564f784c1ec874feb68db7fc21ab9050a46896
kevinmid3 <kevinmid3@gmail.com>
2023-02-02 05:34:04 -0800

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,70 +1,37 @@
-#include "main.h"
+nclude "main.h"
 
 /**
- * binary_to_uint - convert binary to unsigned int
- * @b: binary
- * Return: unsigned int
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
  */
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int ui;
+	int len, base_two;
 
-	int len = 0, i;
-	unsigned int sum = 0;
+	if (!b)
+		return (0);
 
-	if (b == NULL)
-		return (sum);
+	ui = 0;
 
-	/* find string length */
-	while (b[len] != '\0')
-		len++;
-	len -= 1;
+	for (len = 0; b[len] != '\0'; len++)
+		;
 
-	/* iterate string and if '1' then multiply by power of 2 */
-	/* get power of 2 via binary (e.g. 1<<2 = 100 in binary = 4) */
-	i = 0;
-	while (b[i])
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if ((b[i] != '0') && (b[i] != '1'))
-			return (sum);
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
 
-		if (b[i] == '1')
-			sum += (1 * (1 << len));
-		i++;
-		len--;
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
 
-	return (sum);
+	return (ui);
 }
-
-/*
- * alternative method not using bitwise but a power of 2
- *
- *
- * unsigned int binary_to_uint(const char *b)
- *{
- *
- *	int len = 0, pow = 1;
- *	unsigned int sum = 0, error = 0;
- *
- *	if (b == NULL)
- *		return (error);
- *
- *	while (b[len] != '\0') // find string length
- *		len++;
- *	len -= 1;
- *
- *	while (len >= 0) // iterate from back of string
- *	{
- *		if ((b[len] != '0') && (b[len] != '1'))
- *			return (error);
- *
- *		if (b[len] == '1') // add appropriate power of 2 if '1'
- *			sum += pow;
- *
- *		pow *= 2; // update power of 2
- *		len--;
- *	}
- *
- *	return (sum);
- *}
- */
