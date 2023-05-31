83a6aed74088a5256f5fa3a2006f3f70d146f13a
yellowduke <yelohduke@gmail.com>
2023-05-31 14:15:30 -0700

    initial commit

---
+++ 0x1A-hash_tables/hash_tables.h
@@ -0,0 +1,21 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct hash_node_s - Node of a hash table
+ * @key: The key, string
+ * The key is unique in the HashTable
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
+ */
+
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Defines the perimeter of the island described in grid """
+
+
+def island_perimeter(grid):
+    """ Return perimeter of grid where "1" is found """
+    perimeter = 0
+    for c in range(len(grid)):
+        for d in range(len(grid[0])):
+            if grid[c][d] == 1:
+                if c == 0 or grid[c - 1][d] == 0:
+                    perimeter += 1
+                if c == (len(grid) - 1) or grid[c + 1][b] == 0:
+                    perimeter += 1
+                if d == 0 or grid[c][d - 1] == 0:
+                    perimeter += 1
+                if d == (len(grid[0]) - 1) or grid[c][d + 1] == 0:
+                    perimeter += 1
+    return perimeter
+
